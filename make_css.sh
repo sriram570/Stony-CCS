357816e41af7cbbd6428b50cc39e22ee2854796a
Shyam Sundar Chandrasekaran <shychandrase@cs.stonybrook.edu>
2016-11-19 03:32:57 +0000

    make_css.sh

---
+++ make_css.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#dependencies
+#md5sha1sum <- "brew install md5sha1sum" using homebrew/linuxbrew
+
+#clone pitchfork
+git clone https://github.com/PacificBiosciences/pitchfork.git
+
+#change directory to pitchfork
+cd pitchfork
+
+#install blasr
+make blasr
+
+#install ccs
+make pbccs
