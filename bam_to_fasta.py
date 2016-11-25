e05802118ce68e30962a9f039017523b4d404e05
Swaminathan Sivaraman <sswaminathan92@gmail.com>
2016-11-25 16:44:34 -0500

    Adding the fasta file generated using bam_to_fasta as well for the sample data
    Also allow oassing an input bam file to the converter script from the command-line.

--- bam_to_fasta.py
+++ bam_to_fasta.py
@@ -1,35 +1,46 @@
+#!/usr/local/bin/python
 import os
+import sys
 import pysam
 
-bamf = pysam.AlignmentFile("/home/ssriram570/Desktop/cse549-compbio/samples/sample_bam.bam", "rb", check_sq=False)
+def main():
+    try:
+        infile = sys.argv[1]
+    except IndexError:
+        sys.exit("Run using bam_to_fasta.py <bam_file>")
 
-# dict for mapping file descriptors to corresponding FASTA files
-fds = {}
+    bamf = pysam.AlignmentFile(infile, "rb", check_sq=False)
 
-# creating a parent directory (if it doesn't exist) containing all FASTA files
-DIR = "fasta_data/"
-if not os.path.exists(DIR):
-    os.makedirs(DIR)
+    # dict for mapping file descriptors to corresponding FASTA files
+    fds = {}
 
-# writing sequences to corresponding FASTA files
-# naming convention for files:
-# 1. file name -> 'sequence id'
-# 2. sequence names inside files -> {qStart}_{qEnd}
-for i, line in enumerate(bamf.fetch(until_eof=True)):
-	seq_name = ">"
-	#print line.query_name
-	q_name = line.query_name.split('/')
-	seq_name = seq_name + q_name[2]
-	if q_name[1] not in fds:
-		file_name = DIR + q_name[1] + ".fa"
-		fds[q_name[1]] = open(file_name, 'a')
-	#print (type(fds[q_name[1]]))
-	fds[q_name[1]].write(seq_name + "\n")
-	fds[q_name[1]].write(line.query_sequence + "\n")
+    # creating a parent directory (if it doesn't exist) containing all FASTA files
+    DIR = "fasta_data/"
+    if not os.path.exists(DIR):
+        os.makedirs(DIR)
 
-# closing all 'opened' FASTA files
-for fd in fds:
-	fds[fd].close()
+    # writing sequences to corresponding FASTA files
+    # naming convention for files:
+    # 1. file name -> 'sequence id'
+    # 2. sequence names inside files -> {qStart}_{qEnd}
+    for i, line in enumerate(bamf.fetch(until_eof=True)):
+        seq_name = ">"
+        #print line.query_name
+        q_name = line.query_name.split('/')
+        seq_name = seq_name + q_name[2]
+        if q_name[1] not in fds:
+            file_name = DIR + q_name[1] + ".fa"
+            fds[q_name[1]] = open(file_name, 'a')
+        #print (type(fds[q_name[1]]))
+        fds[q_name[1]].write(seq_name + "\n")
+        fds[q_name[1]].write(line.query_sequence + "\n")
 
-# closing the BAM file
-bamf.close()
+    # closing all 'opened' FASTA files
+    for fd in fds:
+        fds[fd].close()
+
+    # closing the BAM file
+    bamf.close()
+
+if __name__ == '__main__':
+    main()
